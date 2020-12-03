629dd1bc3274f5bc6a7ed180b05427ffc3b441c9
Iliya <iliyamiroshnichenko88@gmail.com>
2020-12-03 13:24:07 +0200

    Исправил имена переменных дз2

--- js/task/task-1.js
+++ js/task/task-1.js
@@ -8,8 +8,8 @@ console.log(`В списке ${itemsRef.length} категории.`);
 
 itemsRef.forEach(item => {
     const titleRef = item.querySelector("h2");
-    const subItems = item.querySelectorAll("li");
+    const subItemsRef = item.querySelectorAll("li");
     console.log(
-      `Категория: ${titleRef.textContent}\nКоличество элементов: ${subItems.length}`
+      `Категория: ${titleRef.textContent}\nКоличество элементов: ${subItemsRef.length}`
     );
 })
