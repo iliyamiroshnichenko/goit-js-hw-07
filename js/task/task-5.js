50751933467b1238ecba1c6462334ea57e438e57
Iliya <iliyamiroshnichenko88@gmail.com>
2020-12-05 19:47:18 +0200

    Начал 7.5

--- js/task/task-5.js
+++ js/task/task-5.js
@@ -1,1 +1,8 @@
+/*Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), 
+подставляет его текущее значение в span#name-output. Если инпут пустой, 
+в спане должна отображаться строка 'незнакомец'. */
 
+const inputRef = document.querySelector('#name-input');
+const textRef = document.querySelector('#name-output');
+
+// inputRef.addEventListener('input', () => textRef.textContent = inputRef.textContent)
