3c327bbac3a4e13e30cf8e551d16d64e749b3bb9
Iliya <iliyamiroshnichenko88@gmail.com>
2020-12-05 20:19:01 +0200

    +

--- js/07-2/02-forms.js
+++ js/07-2/02-forms.js
@@ -1,6 +1,16 @@
 const formRef = document.querySelector('.js-register-form');
 formRef.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(event);
-    console.log("Submit!");
+    // const formElements = event.target.elements;
+    // console.dir(formElements.subscription.value);
+
+    const formData = new FormData(event.target);
+   
+    const submittedData = {};
+    formData.forEach((value, key) => {
+        console.log('value: ', value);
+        console.log('key: ', key);
+        submittedData[key] = value;
+    });
+    console.log(submittedData);
 })
