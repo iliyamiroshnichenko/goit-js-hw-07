56a2d0bb9b7cc94464cf64b997d07f5fb32f7591
Iliya <iliyamiroshnichenko88@gmail.com>
2020-12-06 20:28:59 +0200

    Решил задачу 7.6

--- js/task/task-6.js
+++ js/task/task-6.js
@@ -1,1 +1,23 @@
+/* Напиши скрипт, который бы при потере фокуса на инпуте, 
+проверял его содержимое на правильное количество символов.
+Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
+Если введено подходящее количество, то border инпута становится зеленым, 
+если неправильное - красным.
+*/
 
+const inputRef = document.querySelector("#validation-input");
+const countOfSymbols = +inputRef.dataset.length;
+
+const setClass = (e) => {
+  if (e.target.value.length === countOfSymbols) {
+    e.target.classList.contains("invalid")
+      ? e.target.classList.replace("invalid", "valid")
+      : e.target.classList.add("valid");
+  } else {
+    e.target.classList.contains("valid")
+      ? e.target.classList.replace("valid", "invalid")
+      : e.target.classList.add("invalid");
+  }
+};
+
+inputRef.addEventListener("blur", setClass);
